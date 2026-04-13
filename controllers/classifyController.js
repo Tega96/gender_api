499683744e7b1c21e796915b32ffc813e1fc39cd
Otega Otite <otiteotega@gmail.com>
2026-04-13 07:42:02 +0100

    Refactor to meet specification

--- controllers/classifyController.js
+++ controllers/classifyController.js
@@ -10,45 +10,62 @@
 export const classify = async (req, res) => {
 
     try {
-        // Checks if name parameter is empty
         const name = req.query.name
-        if (!name || name === '') {
-            return res.status(400).json({message: 'Bad request: Please inpute a valid name'})
+
+        // Checks if name parameter is empty
+        if (!name) {
+            return res.status(400).json({
+                status: "error", 
+                message: 'Bad request: Please inpute a valid name'
+            })
         }
-        if (typeof(name) === '') {
-            return res.status(422).json({message: 'Unprocessed Entity'})
+
+
+        // name.trim();
+
+        if (typeof name !== 'string') {
+            return res.status(422).json({
+                status: "error", 
+                message: 'Unprocessed Entity'
+            })
         }
             
         const response = await fetch(`https://api.genderize.io/?name=${name}`);
         
-        if (!response) {
+        if (!response.ok) {
             throw new Error('Error getting file from genderize api')
         }
 
         const data = await response.json()
 
-        if (data.name === null || data.count === 0) {
-            res.json({status: "error", message: "No prediction available for the provided name"})
-        }
-
+        const processTime = new Date().toUTCString();
         const processedName = {
             name: data.name,
             gender: data.gender,
             probability: data.probability,
             sample_size: data.count,
-            is_confident: (data.probability >= 0.7) && (data.count >= 100) ? 'true': 'false',
-            processed_at: Date("now").toString
+            is_confident: (data.probability >= 0.7 && data.count >= 100) ? true: false,
+            processed_at: processTime
         }
 
+        if (processedName.gender === null || processedName.sample_size === 0) {
+            return res.json({
+                status: "error", 
+                message: "No prediction available for the provided name"
+            })
+        }
 
         res.status(200).json({
-            status: 'successful',
+            status: 'success',
             data: processedName,
         })
 
     } catch (error) {
-        console.error(error)
-        res.status(500).json({status: "error", message: "Server error"})
+        console.error(error.message)
+        res.status(500).json({
+            status: "error", 
+            message: "Server error"
+        })
     }
 };
 
